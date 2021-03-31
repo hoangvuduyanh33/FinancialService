edd74489e85a5b2548bd765a760b3795228d4fa1
hoangvuduyanh33 <emanhnhi124@gmail.com>
2021-03-31 13:06:30 +0700

    Change datawarehouse

--- Datawarehouse.sql
+++ Datawarehouse.sql
@@ -15,6 +15,14 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema datawarehouse
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `datawarehouse` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+-- -----------------------------------------------------
+-- Schema financialservice
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema financialservice
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `financialservice` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `datawarehouse` ;
 
 -- -----------------------------------------------------
@@ -39,6 +47,7 @@ CREATE TABLE IF NOT EXISTS `datawarehouse`.`customer_demographic_dimension` (
   `AgeBand` INT NULL DEFAULT NULL,
   `IncomeBand` INT NULL DEFAULT NULL,
   `MaritalStatus` INT NULL DEFAULT NULL,
+  `IsEmployed` TINYINT NULL,
   PRIMARY KEY (`CustomerDemographicKey`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
@@ -52,9 +61,6 @@ CREATE TABLE IF NOT EXISTS `datawarehouse`.`customer_dimension` (
   `CustomerKey` VARCHAR(45) NOT NULL,
   `Name` VARCHAR(45) NULL DEFAULT NULL,
   `DOB` DATETIME NULL DEFAULT NULL,
-  `Job` VARCHAR(45) NULL DEFAULT NULL,
-  `Income` INT NULL DEFAULT NULL,
-  `MaritalStatus` INT NULL DEFAULT NULL,
   `Street` VARCHAR(45) NULL DEFAULT NULL,
   `City` VARCHAR(45) NULL DEFAULT NULL,
   `Province` VARCHAR(45) NULL DEFAULT NULL,
@@ -162,6 +168,116 @@ CREATE TABLE IF NOT EXISTS `datawarehouse`.`loan_transaction_facts` (
   CONSTRAINT `fk_monthly_loan_snapshot_facts_loan_dimension1`
     FOREIGN KEY (`LoanKey`)
     REFERENCES `datawarehouse`.`loan_dimension` (`LoanKey`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+USE `financialservice` ;
+
+-- -----------------------------------------------------
+-- Table `financialservice`.`branch`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `financialservice`.`branch` (
+  `BranchID` VARCHAR(45) NOT NULL,
+  `Address` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`BranchID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `financialservice`.`customer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `financialservice`.`customer` (
+  `ID` VARCHAR(45) NOT NULL,
+  `Name` VARCHAR(45) NULL DEFAULT NULL,
+  `Job` VARCHAR(45) NULL DEFAULT NULL,
+  `DOB` DATETIME NULL DEFAULT NULL,
+  `Address` VARCHAR(45) NULL DEFAULT NULL,
+  `MaritalStatus` INT NULL DEFAULT NULL,
+  `Income` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `financialservice`.`account`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `financialservice`.`account` (
+  `AccountID` VARCHAR(45) NOT NULL,
+  `CreatedDate` DATETIME NULL DEFAULT NULL,
+  `Balance` INT NULL DEFAULT NULL,
+  `CustomerID` VARCHAR(45) NOT NULL,
+  `BranchID` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`AccountID`),
+  INDEX `fk_account_customer_idx` (`CustomerID` ASC) VISIBLE,
+  INDEX `fk_account_branch1_idx` (`BranchID` ASC) VISIBLE,
+  CONSTRAINT `fk_account_branch1`
+    FOREIGN KEY (`BranchID`)
+    REFERENCES `financialservice`.`branch` (`BranchID`),
+  CONSTRAINT `fk_account_customer`
+    FOREIGN KEY (`CustomerID`)
+    REFERENCES `financialservice`.`customer` (`ID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `financialservice`.`loan_category`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `financialservice`.`loan_category` (
+  `CategoryID` VARCHAR(45) NOT NULL,
+  `LifeSpan` INT NULL DEFAULT NULL,
+  `Rate` DOUBLE NULL DEFAULT NULL,
+  `Purpose` VARCHAR(45) NULL DEFAULT NULL,
+  `IsSecured` TINYINT NULL DEFAULT NULL,
+  PRIMARY KEY (`CategoryID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `financialservice`.`loan`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `financialservice`.`loan` (
+  `LoanID` VARCHAR(45) NOT NULL,
+  `AvailableAmount` INT NULL DEFAULT NULL,
+  `StartDate` DATETIME NULL DEFAULT NULL,
+  `AccountID` VARCHAR(45) NOT NULL,
+  `CategoryID` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`LoanID`),
+  INDEX `fk_loan_account1_idx` (`AccountID` ASC) VISIBLE,
+  INDEX `fk_loan_loan_category1_idx` (`CategoryID` ASC) VISIBLE,
+  CONSTRAINT `fk_loan_account1`
+    FOREIGN KEY (`AccountID`)
+    REFERENCES `financialservice`.`account` (`AccountID`),
+  CONSTRAINT `fk_loan_loan_category1`
+    FOREIGN KEY (`CategoryID`)
+    REFERENCES `financialservice`.`loan_category` (`CategoryID`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `financialservice`.`loan_transaction`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `financialservice`.`loan_transaction` (
+  `TransactionID` VARCHAR(45) NOT NULL,
+  `Date` DATETIME NULL DEFAULT NULL,
+  `LoanAmount` INT NULL DEFAULT NULL,
+  `PaidAmount` INT NULL DEFAULT NULL,
+  `LoanID` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`TransactionID`),
+  INDEX `fk_loan_transaction_loan1_idx` (`LoanID` ASC) VISIBLE,
+  CONSTRAINT `fk_loan_transaction_loan1`
+    FOREIGN KEY (`LoanID`)
+    REFERENCES `financialservice`.`loan` (`LoanID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
